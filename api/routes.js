3319c158050d63f92267e90acebeeb2926000a22
Toan bach <ngoctoan@live.com>
2022-10-04 08:03:18 +0700

    Add default entry

--- api/routes.js
+++ api/routes.js
@@ -1,7 +1,12 @@
 'use strict';
-module.exports = function(app) {
+module.exports = function (app) {
   let imagesCtrl = require('./controllers/ImagesController');
 
+
+  app.route('/')
+    .get((req,res) => {
+      res.json('App is running...');
+    });
   app.route('/images/image-to-text')
     .post(imagesCtrl.imageToText);
 };
